1d9b92ce126903b50bfc321d868b47e0ffa80f14
Bengt Ove Sannes <dev@bovesan.com>
2018-02-20 08:53:02 +0100

    Version 1.0

--- gitdir-move.py
+++ gitdir-move.py
@@ -4,6 +4,12 @@ import os
 import sys
 import subprocess
 
+usage = 'Usage: gitdir-move.py search_path1 [search_path2 ...] destination_parent_dir'
+
+if len(sys.argv) < 3:
+	print usage
+	sys.exit(1)
+
 destinationParentDir = os.path.abspath(sys.argv[-1])
 if not os.path.isdir(destinationParentDir):
 	try:
@@ -17,27 +23,37 @@ for arg in sys.argv[1:-1]:
 	for root, folderNames, fileNames in os.walk(arg):
 		for folderName in folderNames:
 			if folderName == '.git':
-				gitFolderPath = os.path.join(root, folderName)
+				gitFolderPath = os.path.abspath(os.path.join(root, folderName))
 				gitFolderPaths.append(gitFolderPath)
-				print 'Found .git folder: %s' % gitFolderPath
-				repoName = os.path.dirname(gitFolderPath)
+				# print 'Found .git folder: %s' % gitFolderPath
+				repoName = os.path.basename(os.path.dirname(gitFolderPath))
 				cmd = ['git', 'config', 'core.workdir', gitFolderPath]
-				print cmd,
-				# configReturnCode = subprocess.call(cmd)
-				# if configReturnCode > 0:
-				# 	print '[FAILED]'
-				# 	continue
-				print '[OK]'
+				# print cmd,
+				configReturnCode = subprocess.call(cmd)
+				if configReturnCode > 0:
+					print cmd, '[FAILED]'
+					continue
+				# print cmd, '[OK]'
 				gitdirPath = os.path.join(destinationParentDir, repoName+'.git')
 				try:
-					# os.rename(gitFolderPath, gitdirPath)
+					os.rename(gitFolderPath, gitdirPath)
 					print '%s -> %s' % (gitFolderPath, gitdirPath)
 				except OSError as e:
-					print e.msg
+					print e.strerror
 					continue
 				gitFileContent = 'gitdir: %s' % gitdirPath
-				print '%s: "%s"' % (gitFolderPath, gitFileContent)
+				try:
+					open(gitFolderPath, 'w').write(gitFileContent)
+				except IOError as e:
+					print '  Error: Could not write to .git file. Rolling back:'
+					try:
+						os.rename(gitdirPath, gitFolderPath)
+						print '  %s -> %s' % (gitFolderPath, gitdirPath)
+					except OSError as e:
+						print e.strerror
+						continue
+				# print '%s: "%s"' % (gitFolderPath, gitFileContent)
 
 if len(gitFolderPaths) == 0:
-	print 'No .git folders found'
+	print 'No .git folders found in %s' % sys.argv[1:-1]
 	sys.exit(1)
